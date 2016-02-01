00101817ec15517fe313656d8c441bd984267c11
Greg Marine <gregmarine@iccnet.org>
2016-02-01 16:54:44 +0000

    Added signup flow and start of message service.

--- app-kneel-space/www/js/services.js
+++ app-kneel-space/www/js/services.js
@@ -4,3 +4,32 @@ angular.module('app.services', [])
   var ref = new Firebase('https://intense-torch-8571.firebaseio.com/');
   return $firebaseAuth(ref);
 }])
+
+.factory('Message', ['$ionicPopup', '$timeout', function($ionicPopup, $timeout) {
+  return {
+    timedAlert: function(title, message, duration) {
+      var alertPopup = $ionicPopup.alert({
+        title: title,
+        template: message
+      });
+      
+      var timeout;
+      switch (duration) {
+        case 'short':
+          timeout = 3000;
+          break;
+          
+        case 'long':
+          timeout = 5000;
+          break;
+        
+        default:
+          timeout = 3000;
+      }
+    
+      $timeout(function() {
+        alertPopup.close(); //close the popup after 3 seconds for some reason
+      }, timeout);
+    }
+  }
+}])
