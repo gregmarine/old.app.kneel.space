432b278099c0c11c58516b9d45d9e306b016bb4d
Greg Marine <gregmarine@iccnet.org>
2016-02-03 00:20:03 +0000

    Added account management

--- app-kneel-space/www/js/controllers.js
+++ app-kneel-space/www/js/controllers.js
@@ -8,11 +8,14 @@ angular.module('app.controllers', [])
       console.log("===Logged In===");
       $scope.authData = authData;
       
-      _usersRef.child(authData.uid).child('provider').set(authData.provider);
-      _usersRef.child(authData.uid).child('name').set(getName(authData));
-      $timeout(function() {
-         $scope.$apply();
-      }, 500);
+      // _usersRef.once('value', function(snapshot) {
+      //   console.log("===Check for existing===");
+      //   if(!snapshot.hasChild(authData.uid)) {
+      //     console.log("===New User===");
+          // _usersRef.child(authData.uid).child('profile').child('name').set(getName(authData));
+          // _usersRef.child(authData.uid).child('profile').child('bio').set("");
+      //  }
+      // });
     } else {
       console.log("===Logged Out===");
     }
@@ -45,6 +48,10 @@ angular.module('app.controllers', [])
 
 .controller('LoginCtrl', function($scope, $state, Auth, Message) {
   var _usersRef = new Firebase('https://intense-torch-8571.firebaseio.com/users');
+  
+  if(Auth.$getAuth()) {
+    $state.go('app.playlists');
+  }
   
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -94,7 +101,7 @@ angular.module('app.controllers', [])
   };
 })
 
-.controller('SignupCtrl', function($scope, $state, $ionicPopup, $timeout, $ionicHistory, Auth, Message) {
+.controller('SignupCtrl', function($scope, $state, $ionicHistory, Auth, Message) {
   var _usersRef = new Firebase('https://intense-torch-8571.firebaseio.com/users');
   
   // With the new view caching in Ionic, Controllers are only called
@@ -132,6 +139,117 @@ angular.module('app.controllers', [])
   
   $scope.cancel = function() {
     $ionicHistory.goBack();
+  };
+})
+
+.controller('AccountCtrl', function($scope, $state, $ionicHistory, $firebaseObject, Auth, Message) {
+  
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  // $scope.$on('$ionicView.enter', function(e) {
+  // });
+  
+  var authData = Auth.$getAuth();
+  var userProfileRef = new Firebase('https://intense-torch-8571.firebaseio.com/users/' + authData.uid + '/profile');
+
+  $scope.profileData = $firebaseObject(userProfileRef);
+  
+  $scope.emailData = {
+    new_email: authData.password.email,
+    email: authData.password.email
+  };
+  
+  $scope.passwordData = {};
+  
+  $scope.saveEmail = function() {
+    $scope.message = null;
+    $scope.error = null;
+    
+    // Update email address
+    if($scope.emailData.email !== $scope.emailData.new_email)
+    {
+      Auth.$changeEmail({
+        oldEmail: $scope.emailData.email,
+        newEmail: $scope.emailData.new_email,
+        password: $scope.emailData.password
+      }).then(function() {
+        $scope.emailData.email = $scope.emailData.new_email;
+        $scope.message = "Email changed successfully!";
+        Message.timedAlert('Success', $scope.message, 'short');
+      }).catch(function(error) {
+        $scope.error = error;
+        Message.timedAlert('Error', $scope.error, 'short');
+      });
+    }
+  };
+  
+  $scope.savePassword = function() {
+    $scope.message = null;
+    $scope.error = null;
+    
+    // Update password
+    if($scope.passwordData.new_password) {
+      if($scope.passwordData.new_password == $scope.passwordData.retype_password) {
+        Auth.$changePassword({
+          email: $scope.emailData.email,
+          oldPassword: $scope.passwordData.password,
+          newPassword: $scope.passwordData.new_password
+        }).then(function() {
+          $scope.passwordData = {};
+          $scope.message = "Password changed successfully!";
+          Message.timedAlert('Success', $scope.message, 'short');
+        }).catch(function(error) {
+          $scope.error = error;
+          Message.timedAlert('Error', $scope.error, 'short');
+        });
+      } else {
+        $scope.error = "The new passwords do not match.";
+        Message.timedAlert('Error', $scope.error, 'short');
+      }
+    }
+  };
+  
+  $scope.deleteAccount = function() {
+    $scope.message = null;
+    $scope.error = null;
+    
+    // 1. Confirm
+    var options = {
+      title: "Delete Account",
+      subTitle: "Are you sure you would like to delete your account?",
+      message: "THIS CANNOT BE UNDONE!",
+      positive_label: "GOOD BYE!",
+      negative_label: "NEVER MIND",
+      callback: function(result) {
+        if(result) {
+          var userRef = new Firebase('https://intense-torch-8571.firebaseio.com/users/' + authData.uid);
+          
+          // 2. Remove user from users
+          var obj = $firebaseObject(userRef);
+          obj.$remove().then(function(ref) {
+            // 3. Remove account from auth
+            Auth.$removeUser({
+              email: $scope.emailData.email,
+              password: $scope.passwordData.password
+            }).then(function() {
+              $scope.message = "Your account was successfully removed!";
+              Message.timedAlert('Success', $scope.message, 'short');
+              Auth.$unauth();
+              $state.go('login');
+            }).catch(function(error) {
+              $scope.error = error;
+              Message.timedAlert('Error', $scope.error, 'long');
+            });
+          }, function(error) {
+            $scope.error = error;
+            Message.timedAlert('Error', $scope.error, 'long');
+          });
+        }
+      }
+    };
+    Message.confirm(options);
   };
 })
 
